df652c71e2a6f5b458e264325bd43fd3d3455135
Roberto1Rodriguez <robertordzw@gmail.com>
2023-03-11 01:01:15 -0600

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DirectorAPP;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new Views.LoginView();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DirectorAPP;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
